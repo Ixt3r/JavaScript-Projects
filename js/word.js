fc209c8d0dc0eabb12d90dd86cfe8114e5f788c9
Estefani <mra.estefani1@gmail.com>
2024-01-01 20:33:46 -0300

    inserção da wordlist

--- js/word.js
+++ js/word.js
@@ -1,10 +1,34 @@
 const wordList = [
   {
-    word: "Java"
-    hint: "Linguagem de programação"
+    word: "Java",
+    hint: "Linguagem de programação",
   },
   {
-    word: "Bahia"
-    hint: "Estado Brasileiro"
-  }
-]
+    word: "Código",
+    hint: "Conjunto de instruções que um computador pode executar",
+  },
+  {
+    word: "Nuvem",
+    hint: "Armazenamento de dados online",
+  },
+  {
+    word: "Byte",
+    hint: "Unidade básica de armazenamento de dados",
+  },
+  {
+    word: "Sensor",
+    hint: "Dispositivo que detecta mudanças no ambiente",
+  },
+  {
+    word: "Backup",
+    hint: "Cópia de segurança de dados para prevenir perda",
+  },
+  {
+    word: "Wifi",
+    hint: "Tecnologia de rede sem fio",
+  },
+  {
+    word: "Pixel",
+    hint: " O menor elemento de uma imagem em tela",
+  },
+];
